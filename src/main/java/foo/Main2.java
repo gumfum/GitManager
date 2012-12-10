64a18106e605f40c58c735d0751f059c3858caeb
Ryohei Takasawa <henshinfukanou@gmail.com>
2012-12-10 15:31:27 +0900

    add sampleout.txt

--- src/main/java/foo/Main2.java
+++ src/main/java/foo/Main2.java
@@ -7,12 +7,24 @@ import java.io.IOException;
 public class Main2 {
   public static void main(String[] args) {
     try {
-      FileReader in = new FileReader("out.txt");
+      FileReader in = new FileReader("sampleout.txt");
       BufferedReader br = new BufferedReader(in);
       String line;
 
       while ((line = br.readLine()) != null) {
-        System.out.println(line);
+        String[] words = line.split(" ");
+
+        if (words[0].contains("****")) {
+          String fileName = words[1].replace('\\', '/');
+          String testName = words[2];
+          System.out.println(fileName);
+          System.out.println(testName);
+        } else if (words[0] != null) {
+          line = line.replace('\\', '/');
+          System.out.println(line);
+        } else {
+          continue;
+        }
       }
 
       br.close();
