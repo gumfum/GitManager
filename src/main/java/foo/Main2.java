93ba7eb3ad7e158215ea0e5cd452a55c9ccb1697
Ryohei Takasawa <henshinfukanou@gmail.com>
2012-12-11 21:39:59 +0900

    update Main2.java. Close #1

--- src/main/java/foo/Main2.java
+++ src/main/java/foo/Main2.java
@@ -7,23 +7,31 @@ import java.io.IOException;
 public class Main2 {
   public static void main(String[] args) {
     try {
+      String root = "";
+
       FileReader in = new FileReader("sampleout.txt");
       BufferedReader br = new BufferedReader(in);
       String line;
 
+      String fileName = new String("");
+      String testName = new String("");
       while ((line = br.readLine()) != null) {
         String[] words = line.split(" ");
 
+        // filename lines
         if (words[0].contains("****")) {
-          String fileName = words[1].replace('\\', '/');
-          String testName = words[2];
+          fileName = words[1].replace('\\', '/').substring(0, words[1].length() - 1);
+          testName = words[2].substring(words[2].lastIndexOf('.') + 1);
           System.out.println(fileName);
           System.out.println(testName);
-        } else if (words[0] != null) {
-          line = line.replace('\\', '/');
-          System.out.println(line);
-        } else {
-          continue;
+        }
+        // coverage data lines
+        else if (words[0].contains(".java")) {
+          System.out.println("data");
+        }
+        // empty lines
+        else {
+          System.out.println("empty line");
         }
       }
 
