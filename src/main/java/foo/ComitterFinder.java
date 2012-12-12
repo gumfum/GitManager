9f6282b4d1ceddcc309837dcb5f4d7914c0181c8
Ryohei Takasawa <henshinfukanou@gmail.com>
2012-12-12 10:30:32 +0900

    update ComitterFinder to make output files. Close #4

--- src/main/java/foo/ComitterFinder.java
+++ src/main/java/foo/ComitterFinder.java
@@ -1,9 +1,15 @@
 package foo;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.jgit.api.BlameCommand;
 import org.eclipse.jgit.api.Git;
@@ -21,6 +27,10 @@ public class ComitterFinder {
   private BlameResult result;
   private BufferedReader fileReader;
 
+  private List<String> list = new ArrayList<String>();
+
+  private List<String> outputList = new ArrayList<String>();
+
   ComitterFinder(Git git) {
     this.git = git;
     command = this.git.blame();
@@ -32,9 +42,13 @@ public class ComitterFinder {
       gitFile = new File(this.root + ".git");
       repository = new FileRepository(gitFile);
       git = new Git(repository);
+
+      File outDir = new File(this.root + "output/");
+      if (!outDir.exists()) {
+        outDir.mkdir();
+      }
     } catch (Exception e) {
-      System.out.println("Error");
-      return;
+      e.printStackTrace();
     }
 
     command = git.blame();
@@ -77,5 +91,56 @@ public class ComitterFinder {
     }
 
     return result.getSourceAuthor(lineNum).getName();
+  }
+
+  public void makeOutputFile(String fileName) {
+    String path = root + "output/" + fileName + ".output.txt";
+    File outputFile = new File(path);
+    try {
+      PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(outputFile)));
+
+      for (String str : outputList) {
+        pw.println(str);
+      }
+
+      pw.close();
+      outputList.clear();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  public void pushOutput(String line) {
+    outputList.add(line);
+  }
+
+  public void push(String methodName, String authorName) {
+    list.add(methodName);
+    list.add(authorName);
+  }
+
+  public void clear() {
+    list.clear();
+  }
+
+  public void makeAuthorList() {
+    File file = new File(root + "output/auth.txt");
+    try {
+      PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+
+      for (int i = 0; i < list.size() / 2; ++i) {
+        pw.print(list.get(2 * i) + " : ");
+        pw.println(list.get(2 * i + 1));
+      }
+
+      pw.close();
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 }
--- src/main/java/foo/Main2.java
+++ src/main/java/foo/Main2.java
@@ -6,38 +6,56 @@ import java.io.IOException;
 
 public class Main2 {
   public static void main(String[] args) {
-    String root = "";
+    String root = "C:/Project/TestSample/";
     ComitterFinder cf = new ComitterFinder(root);
 
     try {
-      FileReader in = new FileReader("sampleout.txt");
-      BufferedReader br = new BufferedReader(in);
+      BufferedReader br = new BufferedReader(new FileReader(root + "testout.txt"));
       String line;
 
       String fileName = new String("");
       String testName = new String("");
+      String authorName = new String("");
+      boolean flag = false;
+
       while ((line = br.readLine()) != null) {
         String[] words = line.split(" ");
 
         // filename lines
         if (words[0].contains("****")) {
+          flag = true;
           fileName = words[1].replace('\\', '/').substring(0, words[1].length() - 1);
           testName = words[2].substring(words[2].lastIndexOf('.') + 1);
           System.out.println(fileName);
           System.out.println(testName);
+
+          cf.setFilePath(fileName);
+          authorName = cf.getAuthorName(testName);
+          System.out.println(authorName);
+
+          cf.push(testName, authorName);
         }
         // coverage data lines
         else if (words[0].contains(".java")) {
-          System.out.println("data");
+          System.out.println("** data **");
+          cf.pushOutput(authorName + " : " + line);
         }
         // empty lines
         else {
-          System.out.println("empty line");
+          System.out.println("** empty line **");
+          if (flag) {
+            cf.makeOutputFile(testName);
+          }
+          flag = false;
         }
+
+        cf.makeAuthorList();
+      }
+      if (flag) {
+        cf.makeOutputFile(testName);
       }
 
       br.close();
-      in.close();
     } catch (IOException e) {
       System.out.println(e);
     }
